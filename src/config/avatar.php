f692e0b3ec72200f1b13a04f307737beb298b146
zj <2223644122@qq.com>
2019-07-11 20:43:28 +0800

    打印名字

---
+++ src/config/avatar.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/7/11
+ * Time: 17:19
+ */
+return   [
+    'type' => 'png', // jpeg|png|gif|bmp
+    'width' => '100',
+    'height' => '100',
+    'size' => '26',
+    'font_file' => public_path() . '/fonts/WawaSC-Regular.otf',
+];
